582a108b3c5eb4a06971c335e8c8ed41b1064155
peter Griffin <114193330+bobyjumkbjgu@users.noreply.github.com>
2024-10-03 20:10:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,15 +14,15 @@ searchInput.addEventListener("input", e => {
   })
 })
 
-fetch("https://jsonplaceholder.typicode.com/users")
+fetch("https://bobyjumkbjgu.github.io/peteFlixV3//Moives")
   .then(res => res.json())
   .then(data => {
     users = data.map(user => {
       const card = userCardTemplate.content.cloneNode(true).children[0]
       const header = card.querySelector("[data-header]")
       const body = card.querySelector("[data-body]")
-      header.textContent = user.name
-      body.textContent = user.email
+      header.textContent = name.moive
+      body.textContent = name.moive
       userCardContainer.append(card)
       return { name: user.name, email: user.email, element: card }
     })
